a91acc4f66da6f8cf98a4a403e693b64528237cf
Randy Faison <randyfaison@gmail.com>
2021-03-25 15:24:25 -0400

    input boxes added, local storage updated

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// var calendardisplay = $('#calendar-display');
+/*// var calendardisplay = $('#calendar-display');
 
 //  function dispcalendar(){
 //      var date = moment().format('dddd, MMMM Do');
@@ -12,23 +12,61 @@
 
 
 //making save buttons clickable 
-$(".btn1").click(function(){
-console.log("clicked");
 
-})
 
 //input items into schedule - input tag(html-59)/textarea tag
 
 
 //to local storage
+saveSchedule();
+
+function saveSchedule(){
+    $('#9 .w-75').val(localStorage.getItem('9'));
+    //document.getElementById("ct");
+    var content =  $('.w-75').val();
+    var time =  $('#9'); 
+
+
+
+    if (!content || !time){
+        return;
+    }
+    
+//console.log("done")
+
+}
+$(".btn1").click(function(){
+    console.log("clicked");
+    
+    var content =  $('.w-75').val();
+    var time =  $('#9');
+
+    console.log(time);
+
+
+    
+        
+    
+    localStorage.setItem(time , content);
+    
+    
+    
+    
+    })
+
 
 //function to change colors - moment library
 //var now = moment().hour(); 
-var now = 17;
-console.log(now);
+
+
+// var now = 17;
+// console.log(now);
+
+
 //create loop for table - loop through each row of table (hour) get value
 //current hour - using data attribute each
 
 
 
 //uses current hour to evaluate colors of table row
+*/
